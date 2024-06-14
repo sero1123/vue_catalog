9e9e6cfd89e0aa61852d6d69eb1be7ff2adb06bb
Gunko Yuri <senior.prozevs@yandex.ru>
2024-06-14 17:33:48 +0300

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Changes to be committed: #	modified:   public/index.html #	modified:   src/App.vue #	deleted:    src/components/HelloWorld.vue #	new file:   src/components/MyCatalog/MyCatalog.vue #

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <my-catalog></my-catalog>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyCatalog from "@/components/MyCatalog/MyCatalog";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    'my-catalog': MyCatalog
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/MyCatalog/MyCatalog.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>catalog</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "MyCatalog"
+}
+</script>
+
+<style scoped>
+
+
+</style>
